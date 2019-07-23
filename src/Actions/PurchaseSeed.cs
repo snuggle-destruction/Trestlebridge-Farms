291cfc13579e81a2117af99e3a1859666ba36292
Joey Driscoll <driscoll.recording@gmail.com>
2019-07-23 14:36:03 -0500

    create facility and purchase seeds now spit out a message to tell you to make a selection if you just hit enter without making a selection

--- src/Actions/PurchaseSeed.cs
+++ src/Actions/PurchaseSeed.cs
@@ -20,107 +20,120 @@ namespace Trestlebridge.Actions
             Console.Write("> ");
             string choice = Console.ReadLine();
 
-            switch (Int32.Parse(choice))
+            if (choice != "")
             {
-                case 1:
-                    if (farm.PlowedFields.Count != 0)
-                    {
-                        ChoosePlowedField.CollectInput(farm, new Sesame());
-                        break;
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("\n\n\n");
-                        Console.WriteLine("There ain't no field for to plantin' your Sesames!");
-                        Console.WriteLine("\n\n");
-                        Console.WriteLine("Press enter to continue");
-                        Console.ReadLine();
-                        break;
-                    }
-                case 2:
-                    if (farm.PlowedFields.Count != 0)
-                    {
-                        ChoosePlowedField.CollectInput(farm, new Oat());
-                        break;
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        Console.WriteLine("\n\n\n");
-                        Console.WriteLine("There ain't no field for to plantin' your Oats!");
-                        Console.WriteLine("\n\n");
-                        Console.WriteLine("Press enter to continue");
-                        Console.ReadLine();
-                        break;
-                    }
-                case 3:
-                    Console.Clear();
-                    Console.WriteLine("1. Plowed Field");
-                    Console.WriteLine("2. Natural Field");
-                    Console.WriteLine();
-                    Console.WriteLine("What type of field do you want to plant your sunflowers?");
-
-                    Console.Write("> ");
-                    string choice2 = Console.ReadLine();
-
-                    switch (Int32.Parse(choice2))
-                    {
-                        case 1:
-                            if (farm.PlowedFields.Count != 0)
-                            {
-                                ChoosePlowedField.CollectInput(farm, new Sunflower());
-                                break;
-                            }
-                            else
-                            {
-                                Console.Clear();
-                                Console.WriteLine("\n\n\n");
-                                Console.WriteLine("There ain't no field for to plantin' your Sunflowers!");
-                                Console.WriteLine("\n\n");
-                                Console.WriteLine("Press enter to continue");
-                                Console.ReadLine();
-                                break;
-                            }
-                        case 2:
-                            if (farm.NaturalFields.Count != 0)
-                            {
-                                ChooseNaturalfield.CollectInput(farm, new Sunflower());
-                                break;
-                            }
-                            else
-                            {
-                                Console.Clear();
-                                Console.WriteLine("\n\n\n");
-                                Console.WriteLine("There ain't no field for to plantin' your Sunflowers!");
-                                Console.WriteLine("\n\n");
-                                Console.WriteLine("Press enter to continue");
-                                Console.ReadLine();
-                                break;
-                            }
-                        default:
+                switch (Int32.Parse(choice))
+                {
+                    case 1:
+                        if (farm.PlowedFields.Count != 0)
+                        {
+                            ChoosePlowedField.CollectInput(farm, new Sesame());
                             break;
-
-                    }
-                    break;
-                case 4:
-                    if (farm.NaturalFields.Count != 0)
-                    {
-                        ChooseNaturalfield.CollectInput(farm, new Wildflower());
-                        break;
-                    }
-                    else
-                    {
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n\n\n");
+                            Console.WriteLine("There ain't no field for to plantin' your Sesames!");
+                            Console.WriteLine("\n\n");
+                            Console.WriteLine("Press enter to continue");
+                            Console.ReadLine();
+                            break;
+                        }
+                    case 2:
+                        if (farm.PlowedFields.Count != 0)
+                        {
+                            ChoosePlowedField.CollectInput(farm, new Oat());
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n\n\n");
+                            Console.WriteLine("There ain't no field for to plantin' your Oats!");
+                            Console.WriteLine("\n\n");
+                            Console.WriteLine("Press enter to continue");
+                            Console.ReadLine();
+                            break;
+                        }
+                    case 3:
                         Console.Clear();
-                        Console.WriteLine("\n\n\n");
-                        Console.WriteLine("There ain't no field for to plantin' your Wildflowers!");
-                        Console.WriteLine("\n\n");
-                        Console.WriteLine("Press enter to continue");
-                        Console.ReadLine();
+                        Console.WriteLine("1. Plowed Field");
+                        Console.WriteLine("2. Natural Field");
+                        Console.WriteLine();
+                        Console.WriteLine("What type of field do you want to plant your sunflowers?");
+
+                        Console.Write("> ");
+                        string choice2 = Console.ReadLine();
+
+                        switch (Int32.Parse(choice2))
+                        {
+                            case 1:
+                                if (farm.PlowedFields.Count != 0)
+                                {
+                                    ChoosePlowedField.CollectInput(farm, new Sunflower());
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("\n\n\n");
+                                    Console.WriteLine("There ain't no field for to plantin' your Sunflowers!");
+                                    Console.WriteLine("\n\n");
+                                    Console.WriteLine("Press enter to continue");
+                                    Console.ReadLine();
+                                    break;
+                                }
+                            case 2:
+                                if (farm.NaturalFields.Count != 0)
+                                {
+                                    ChooseNaturalfield.CollectInput(farm, new Sunflower());
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("\n\n\n");
+                                    Console.WriteLine("There ain't no field for to plantin' your Sunflowers!");
+                                    Console.WriteLine("\n\n");
+                                    Console.WriteLine("Press enter to continue");
+                                    Console.ReadLine();
+                                    break;
+                                }
+                            default:
+                                break;
+
+                        }
                         break;
-                    }
-                default:
-                    break;
+                    case 4:
+                        if (farm.NaturalFields.Count != 0)
+                        {
+                            ChooseNaturalfield.CollectInput(farm, new Wildflower());
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\n\n\n");
+                            Console.WriteLine("There ain't no field for to plantin' your Wildflowers!");
+                            Console.WriteLine("\n\n");
+                            Console.WriteLine("Press enter to continue");
+                            Console.ReadLine();
+                            break;
+                        }
+                    default:
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("You entered nothing. And that's wrong. You're bad.");
+                Console.WriteLine("\n\n");
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
             }
         }
     }
